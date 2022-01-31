17a5f57bfe2feecd8d65aab229946e99b6b3451a
Andreea1696 <andreeapetrescu16@gmail.com>
2022-01-31 16:59:48 +0100

    Create store route

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,4 @@ Route::get('/', 'HomeController@home') -> name('home');
 Route::get('/user/show/{id}', 'HomeController@show') -> name('show');
 
 Route::get('user/create', 'HomeController@create') -> name('create');
+Route::post('user/store', 'HomeController@store') -> name('store');
