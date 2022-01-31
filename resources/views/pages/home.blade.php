bc777584bf93bb36d8016a65c8f1f907a4ecb193
Andreea1696 <andreeapetrescu16@gmail.com>
2022-01-31 16:10:53 +0100

    Print users list in home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.main-layout')
 @section('content')
-    <h2>
-        content here
-    </h2>
+    <section>
+
+        <h2>Users list:</h2>
+
+        <ul>
+            @foreach ($users as $user)
+
+                <li>
+                    <a href="#"> 
+                        {{ $user -> name }} {{ $user -> lastname }}
+                    </a> 
+                </li>
+                
+            @endforeach
+        </ul>
+    </section>
 @endsection
