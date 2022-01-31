557bb96b342ae2fe3a99aad5cef7b16f13435a31
Andreea1696 <andreeapetrescu16@gmail.com>
2022-01-31 16:35:56 +0100

    Add create route in web

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@home') -> name('home');
 Route::get('/user/show/{id}', 'HomeController@show') -> name('show');
+
+Route::get('user/create', 'HomeController@create') -> name('create');
