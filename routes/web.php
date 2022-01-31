bef492132d9ee232e50ae7294a49b7938021b96c
Andreea1696 <andreeapetrescu16@gmail.com>
2022-01-31 17:32:16 +0100

    Create edit and update routes in web

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,6 @@ Route::get('/user/show/{id}', 'HomeController@show') -> name('show');
 
 Route::get('user/create', 'HomeController@create') -> name('create');
 Route::post('user/store', 'HomeController@store') -> name('store');
+
+Route::get('user/edit/{id}', 'HomeController@edit') -> name('edit');
+Route::post('user/update/{id}', 'HomeController@update') -> name('update');
